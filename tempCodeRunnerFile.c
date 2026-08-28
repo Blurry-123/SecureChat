7aec43801eb97bd3655df1c0ff36899c7327fd66
saswatitripathy09-jpg <saswatitripathy09@gmail.com>
2026-08-28 23:50:48 +0530

    Added my crypto and client code

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include "crypto.h"
+#include <string.h>
+void xor_crypt(char *data, int length, const char *key)
+{
+    int keyLen = (int)strlen(key);
+    if (keyLen <= 0)
+        return;
+    for (int index = 0; index < length; index++)
+    {
+        int keyIndex = index % keyLen;
+        data[index] = data[index] ^ key[keyIndex];
+    }
+}
